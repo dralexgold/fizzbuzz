e347afd7e368c0a3fec7002d08717a3806bcfc04
Alex Gold <alex@alexgold.net>
2015-08-08 16:06:41 +1000

    first draft finished

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ $(document).ready(function() {
 	console.log('loaded');
 	$('button').click(function(){
 		console.log('clicked');
-		$('body').append("text");
+		for (var myNum = 1; myNum < 101; myNum++){
+  			if (myNum % 15 === 0) {
+    			$('.fizzbuzz').append('FizzBuzz<br />');	
+  			}
+  			else if (myNum % 3 === 0) {
+      			$('.fizzbuzz').append('Fizz<br />');
+    		}
+  			else if (myNum % 5 === 0) {
+    			$('.fizzbuzz').append('Buzz<br />');
+  			}
+  			else {
+    			$('.fizzbuzz').append(myNum + '<br />');
+  			}
+		}	
 	});
 });
